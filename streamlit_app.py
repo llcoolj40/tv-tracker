c62ae0a1973e123fd8ff16b943e94b13c6be17b8
llcoolj40 <lee@leejordan.co.uk>
2026-04-30 16:57:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 # --- CONFIG ---
 TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmU4NWRkZmQ5NzUyZDUwM2ExNGZhNTYxYTQyZmFjNiIsIm5iZiI6MTc3Njg5MjA3MS41NzksInN1YiI6IjY5ZTkzOGE3NDJmYmRjNjJiYzBmNGE3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3YkaU1hutH9ZYDpJuBumFdajiSbfIZ8jhFpG1rlrFCg"
 HEADERS = {"Authorization": f"Bearer {TMDB_TOKEN}"}
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1LEoZ_C61NQz7HrnGn5uZ5ofpYDNUH0kLAaULVtY1O3I/"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1LEoZ_C61NQz7HrnGn5uZ5ofpYDNUH0kLAaULVtY1O3I"
 
 st.set_page_config(page_title="BingeTracker Elite", page_icon="📺", layout="wide")
 
